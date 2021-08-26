e3fbe8356fb8636162362a0ecd06ed3356c8e47c
96sMicks <melkiasae@hotmail.com>
2021-08-26 22:49:41 +0000

    refactored for edge cases

--- timer1.js
+++ timer1.js
@@ -1,7 +1,24 @@
 const getTimeArrayInSeconds = process.argv.slice(2); // Gets an array of time inputed in the command line
 
-const filteredArray = getTimeArrayInSeconds.filter(num => num > 0);
-// console.log(filteredArray)
+// const filteredArray = getTimeArrayInSeconds.filter(num => num > 0);
+const filteredArray = getTimeArrayInSeconds.filter(x => {
+  if (isNaN(x) || x < 0 ) {
+    return false
+  } else {
+    return true
+  }   
+})
+
+// const arrays = [1, 2, 3]
+// const newArrays =  arrays.filter(x => {
+//   if (x > 1) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// })
+
+console.log(filteredArray);
 
 
 const timeConverterArray = filteredArray.map( (num) => Number(num)*1000 );
@@ -12,10 +29,6 @@ const timeConverterArray = filteredArray.map( (num) => Number(num)*1000 );
 
 timer = function(times) {
   for (const beepTime of times) {
-  if (times.length = 0) {
-    console.log("No numbers inputed");
-  }
-    // console.log(times);
     setTimeout(() => {
       process.stdout.write('\x07');
     }, beepTime);
